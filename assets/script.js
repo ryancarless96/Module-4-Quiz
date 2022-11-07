b7ca54a5ff8b9fedd635a8d517998ba019efd858
Ryan Carless <ryancarless96@gmail.com>
2022-11-07 15:57:59 -0500

    commit changes

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
-var pass =document.querySelector(".pass");
-var fail = document.querySelector(".fail");
-var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector("#start-button");
+var resetButton = document.querySelector(".reset-button");
+var score = document.querySelector(".score");
 
 
-var name = "";
+var timerElement = document.querySelector(".timer-count");
+var passQuiz = document.querySelector(".pass");
+var failQuiz = document.querySelector(".fail");
+
 var score = 0;
 var passCounter = 0;
 var failCounter = 0;
@@ -12,42 +14,96 @@ var timer;
 var timerCount;
 
 
+var answers = ["cd", "touch index.html", "ls", "git commit -m", "git clone", "git pull origin main", "mkdir", "git add -A"];
+
 function init() {
   getPass();
   getFails();
 }
 
 function startQuiz() {
-  timerCount=60;
+  console.log('Begin quiz')
+  timerCount = 60;
+
   startButton.disabled = true;
-  renderBlanks()
   startTimer()
 }
 
+
+function selectAnswer() {
+  var questions = [
+    {
+      question1: "What adds the file to be commited?",
+      answers: {
+        "touch index.html":"incorrect!", 
+        "ls": "incorrect!",
+        "mkdir":"incorrect!",
+        "git add-A": "correct!",
+      },
+    },
+    {
+      question2: "How do you create a new folder?",
+      answers: {
+        "ls": "incorrect!", 
+        "mkdir": "incorrect!",
+        "git clone": "incorrect!",
+        "cd": "correct!",
+      },
+    },
+    {
+      question3: "What do we use to clone the repository?",
+      answers: {
+        "cd": "incorrect!",
+        "touch index.html": "incorrect!",
+        "ls":"incorrect!",
+        "git clone": "correct!",
+      },
+    },
+    {
+      question4: "How do you get the latest from the repo?",
+      answers: {
+        "git commit -m": "incorrect!",
+        "ls": "incorrect!",
+        "git status": "incorrect!",
+        "git pull origin main": "correct!",
+      },
+    }, 
+  ];
+  return questions;
+}
+// var quiz = [
+//   [1, "What adds the file to be commited?", "git add -A"],
+//   [2, "How do you create a new folder?", "cd"],
+//   [3, "What do we use to clone the repository?", "git clone"],
+//   [4, "How do you get the latest from the repo?", "git pull origin main"]
+// ];
+
+
+
 function passQuiz() {
-  wordBlank.textContent = "You Passed!"
+  wordBlank.textContent = "You Passed!" ;
   passCounter++
   startButton.disabled =
-  setPasses()
+    setPasses()
 }
 
 function failQuiz() {
-  wordBlank.textContent = "You Failed!"
+  wordBlank.textContent = "You Failed!" ;
   failCounter++
-  startButton.disabled = 
-  setFails()
+  startButton.disabled =
+    setFails()
 }
 
 function startTimer() {
-  timer = setInterval(function() {
+  timer = setInterval(function () {
     timerCount--;
     timerElement.textContent = timerCount;
-    if(timerCount >= 0){
-      if (isPass && timerCount >0) {
+    if (timerCount >= 0) {
+      if (isPass && timerCount > 0) {
         clearInterval(timer);
         passQuiz()
       }
-    } 
+    }
     if (timerCount === 0) {
       clearInterval(timer);
       failQuiz();
@@ -55,13 +111,13 @@ function startTimer() {
   })
 }
 
-function highScores() {
+function score() {
   chosenScore = scores[Math.floor(Math.random() * score.length)];
   numbersInChosenScore = chosenScore.split("");
   numScores = numbersInChosenScore.length;
   highScores = []
-  
-  for(var i = 0; i < numScores; i++) {
+
+  for (var i = 0; i < numScores; i++) {
     highScores.push("");
   }
   highScores.textContent = highScores.join("")
@@ -73,16 +129,16 @@ function setPasses() {
   localStorage.setItem("passCount", passCounter);
 }
 
-function setFails(){
+function setFails() {
   fail.textContent = failCounter;
-  localStorage.setItem("failCount",failCounter);
+  localStorage.setItem("failCount", failCounter);
 }
 
 function getPasses() {
   var storedPasses = localStorage.getItem("passCount");
   if (storedPasses === null) {
     passCounter = 0;
-  }else {
+  } else {
     passCounter = storedPasses;
   }
   pass.textContent = passCounter;
@@ -91,30 +147,16 @@ function getFails() {
   var storedFails = localStorage.get("failCount");
   if (storedFails === null) {
     failCounter = 0;
-  }else {
+  } else {
     failCounter = storedFails;
   }
   fail.textContent = failCounter;
 }
 
-function pass() {
-}
-
-function myFunction() {
-  ("Have a nice day!")
-}
-
-pass.addEventListener("click", startQuiz);
-
-fail.addEventListener("click",startQuiz);
-
-startButton.addEventListener("click", startQuiz);
-
-timerElement.addEventListener("click", startQuiz);
+startButton.addEventListener("click", startQuiz)
 
 init();
 
-var resetButton = document.querySelector(".reset-button");
 
 function resetQuiz() {
   passCounter = 0;
@@ -123,5 +165,5 @@ function resetQuiz() {
   setPasses()
   setFails()
 
-  resetButton.addEventListener("click",resetQuiz);
+  resetButton.addEventListener("click", resetQuiz);
 }
